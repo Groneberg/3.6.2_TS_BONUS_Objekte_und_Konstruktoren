930ac6cce4c35fc550d1b5620fa9e02aa495c916
Stefan Groneberg <stefan.groneberg@gmx.de>
2025-06-26 16:44:18 +0200

    fixed typo

--- bonus.dart
+++ bonus.dart
@@ -40,96 +40,96 @@ class Rectangle {
 }
 
 // Bonus Task 6
-class Autor {
+class Author {
   String name;
   int birthYear;
 
-  Autor(this.name, this.birthYear);
+  Author(this.name, this.birthYear);
 
   @override
   String toString() {
-    return 'Autor: $name, Geburtsjahr: $birthYear';
+    return 'Author: $name, Geburtsjahr: $birthYear';
   }
 }
 
-class Buch {
+class Book {
   String bookTitle;
   int pages;
-  Autor autor;
+  Author author;
 
-  Buch(this.bookTitle, this.pages, this.autor);
+  Book(this.bookTitle, this.pages, this.author);
 
   @override
   String toString() {
-    return 'Buch: $bookTitle, Seitenzahl: $pages, Autor: ${autor.toString()}';
+    return 'Buch: $bookTitle, Seitenzahl: $pages, Autor: ${author.toString()}';
   }
 }
 
 void main() {
-  // Bonus Task 2
-  Button b = Button(
-    'In den Warenkorb',
-    width: 200,
-    height: 100,
-    radius: 10,
-    fontSize: 16,
-    padding: 20,
-  );
+  // // Bonus Task 2
+  // Button b = Button(
+  //   'In den Warenkorb',
+  //   width: 200,
+  //   height: 100,
+  //   radius: 10,
+  //   fontSize: 16,
+  //   padding: 20,
+  // );
 
-  // Bonus Task 4
-  Rectangle rect = Rectangle(10, 20);
-  print(
-    "Bei einem x von ${rect.x} und y von ${rect.y} ist die Fläche: ${rect.getSurfaceArea()}",
-  );
-  print(
-    "Bei einem x von ${rect.x} und y von ${rect.y} ist der Umfang: ${rect.getCircumference()}",
-  );
-  rect.x = 20;
-  print(
-    "Bei einem x von ${rect.x} und y von ${rect.y} ist die Fläche: ${rect.getSurfaceArea()}",
-  );
-  print(
-    "Bei einem x von ${rect.x} und y von ${rect.y} ist der Umfang: ${rect.getCircumference()}",
-  );
+  // // Bonus Task 4
+  // Rectangle rect = Rectangle(10, 20);
+  // print(
+  //   "Bei einem x von ${rect.x} und y von ${rect.y} ist die Fläche: ${rect.getSurfaceArea()}",
+  // );
+  // print(
+  //   "Bei einem x von ${rect.x} und y von ${rect.y} ist der Umfang: ${rect.getCircumference()}",
+  // );
+  // rect.x = 20;
+  // print(
+  //   "Bei einem x von ${rect.x} und y von ${rect.y} ist die Fläche: ${rect.getSurfaceArea()}",
+  // );
+  // print(
+  //   "Bei einem x von ${rect.x} und y von ${rect.y} ist der Umfang: ${rect.getCircumference()}",
+  // );
 
-  // Bonus Task 5
-  Rectangle rect2 = Rectangle(5, 6);
-  print(
-    "Bei einem x von ${rect2.x} und y von ${rect2.y} ist die Fläche: ${rect2.getSurfaceArea()}",
-  );
-  print(
-    "Bei einem x von ${rect2.x} und y von ${rect2.y} ist der Umfang: ${rect2.getCircumference()}",
-  );
-  rect2.scale(0.5);
-  print(
-    "Bei einem x von ${rect2.x} und y von ${rect2.y} ist die Fläche: ${rect2.getSurfaceArea()}",
-  );
-  print(
-    "Bei einem x von ${rect2.x} und y von ${rect2.y} ist der Umfang: ${rect2.getCircumference()}",
-  );
-  rect2.scale(4);
-  print(
-    "Bei einem x von ${rect2.x} und y von ${rect2.y} ist die Fläche: ${rect2.getSurfaceArea()}",
-  );
-  print(
-    "Bei einem x von ${rect2.x} und y von ${rect2.y} ist der Umfang: ${rect2.getCircumference()}",
-  );
+  // // Bonus Task 5
+  // Rectangle rect2 = Rectangle(5, 6);
+  // print(
+  //   "Bei einem x von ${rect2.x} und y von ${rect2.y} ist die Fläche: ${rect2.getSurfaceArea()}",
+  // );
+  // print(
+  //   "Bei einem x von ${rect2.x} und y von ${rect2.y} ist der Umfang: ${rect2.getCircumference()}",
+  // );
+  // rect2.scale(0.5);
+  // print(
+  //   "Bei einem x von ${rect2.x} und y von ${rect2.y} ist die Fläche: ${rect2.getSurfaceArea()}",
+  // );
+  // print(
+  //   "Bei einem x von ${rect2.x} und y von ${rect2.y} ist der Umfang: ${rect2.getCircumference()}",
+  // );
+  // rect2.scale(4);
+  // print(
+  //   "Bei einem x von ${rect2.x} und y von ${rect2.y} ist die Fläche: ${rect2.getSurfaceArea()}",
+  // );
+  // print(
+  //   "Bei einem x von ${rect2.x} und y von ${rect2.y} ist der Umfang: ${rect2.getCircumference()}",
+  // );
 
   // Bonus Task 6
-  Autor autor1 = Autor('Stephen King', 1947);
-  Autor autor2 = Autor('Joanne K. Rowling', 1965);
+  Author author1 = Author('Stephen King', 1947);
+  Author author2 = Author('Joanne K. Rowling', 1965);
 
-  Buch buch1 = Buch('Es', 1376, autor1);
-  Buch buch2 = Buch('Harry Potter und der Stein der Weisen', 336, autor2);
-  Buch buch3 = Buch('Shining', 447, autor1);
+  Book book1 = Book('Es', 1376, author1);
+  Book book2 = Book('Harry Potter und der Stein der Weisen', 336, author2);
+  Book book3 = Book('Shining', 447, author1);
 
   print(
-    "Das Buch heißt ${buch1.bookTitle} von ${buch1.autor.name}. Es hat ${buch1.pages} Seiten.",
+    "Das Buch heißt ${book1.bookTitle} von ${book1.author.name}. Es hat ${book1.pages} Seiten.",
   );
   print(
-    "Das Buch heißt ${buch2.bookTitle} von ${buch2.autor.name}. Es hat ${buch2.pages} Seiten.",
+    "Das Buch heißt ${book2.bookTitle} von ${book2.author.name}. Es hat ${book2.pages} Seiten.",
   );
   print(
-    "Das Buch heißt ${buch3.bookTitle} von ${buch3.autor.name}. Es hat ${buch3.pages} Seiten.",
+    "Das Buch heißt ${book3.bookTitle} von ${book3.author.name}. Es hat ${book3.pages} Seiten.",
   );
 }
